f1a1cb1cf4c39fd80eff4d9db0f6a6078c81da6b
SHREEANSH GUPTA <shreeansh.gupta@mercedes-benz.com>
2023-05-28 15:18:39 +0530

    check for backend

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const path = require('path');
-const { app, BrowserWindow, ipcMain, dialog } = require('electron');
-const sqlite = require('sqlite3').verbose();
+const path = require("path");
+const { app, BrowserWindow, ipcMain, dialog } = require("electron");
+const sqlite = require("sqlite3").verbose();
 
-const userAppData = app.getPath('appData');
-const dbPath = path.join(userAppData, 'database.db');
-const isDev = process.env.NODE_ENV !== 'production';
+const userAppData = app.getPath("appData");
+const dbPath = path.join(userAppData, "database.db");
+const isDev = process.env.NODE_ENV !== "production";
 
-const db = new sqlite.Database(dbPath, err => {
+const db = new sqlite.Database(dbPath, (err) => {
   if (err) {
     console.error(err);
   }
@@ -15,11 +15,11 @@ const db = new sqlite.Database(dbPath, err => {
 
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
-    title: 'Billing App',
+    title: "Billing App",
     width: isDev ? 1000 : 700,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname, './preload.js'),
+      preload: path.join(__dirname, "./preload.js"),
     },
   });
 
@@ -27,57 +27,68 @@ function createMainWindow() {
     mainWindow.webContents.openDevTools();
   }
 
-  mainWindow.loadFile(path.join(__dirname, './public/index.html'));
+  mainWindow.loadFile(path.join(__dirname, "./public/index.html"));
 }
 
-ipcMain.on('findUser', (event, data) => {
-  db.get(`SELECT * FROM users WHERE phoneNumber = ?`, [data], (err, rows) => {
+ipcMain.on("findUser", (event, data) => {
+  db.get(`SELECT * FROM users WHERE phoneNumber = ?`, [data], (err, row) => {
     if (err) {
       console.error(err);
     } else {
-      if (rows) {
-        const originalDate = new Date(rows['date']);
-        const now = new Date();
-
-        diffDate = Math.abs(now - originalDate);
-        totalDays = Math.ceil(diffDate / (1000 * 60 * 60 * 24));
-        if (totalDays > 4) {
-          rows['newPrice'] = 'Rs. 200';
-        } else {
-          rows['newPrice'] = 'Rs. 0';
-        }
+      if (row) {
+        db.get(
+          `SELECT procedure_name FROM procedures WHERE customer_id = ?`,
+          [row["phoneNumber"]],
+          (err, procInfo) => {
+            if (err) {
+              console.error(err);
+            }
+            console.log(procInfo);
+          }
+        );
       }
-      console.log(rows);
-      event.sender.send('userInfoResult', rows);
+      console.log(row);
+      event.sender.send("userInfoResult", row);
     }
   });
 });
 
-ipcMain.on('formSubmit', (event, data) => {
-  if (Object.keys(data).length < 14) {
-    console.error('Incomplete data');
+ipcMain.on("formSubmit", (event, data) => {
+  if (Object.keys(data).length < 5) {
+    console.error("Incomplete data");
   } else {
     console.log(data);
     db.run(
-      `INSERT INTO users (firstName, lastName, address, district, state, phoneNumber, time, date, pincode, sex, age, billNumber, initials, newPrice)
-      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+      `INSERT OR REPLACE INTO users (firstName, lastName, address, district, state, phoneNumber, time, date, pincode, sex, age, billNumber, initials)
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
       [
-        data['firstName'],
-        data['lastName'],
-        data['address'],
-        data['district'],
-        data['state'],
-        data['phoneNumber'],
-        data['time'],
-        data['date'],
-        data['pincode'],
-        data['sex'],
-        data['age'],
-        data['billNumber'],
-        data['initials'],
-        data['newPrice'],
+        data["firstName"],
+        data["lastName"],
+        data["address"],
+        data["district"],
+        data["state"],
+        data["phoneNumber"],
+        data["time"],
+        data["date"],
+        data["pincode"],
+        data["sex"],
+        data["age"],
+        data["billNumber"],
+        data["initials"],
       ],
-      err => {
+
+      (err) => {
+        if (err) {
+          console.error(err);
+        }
+      }
+    );
+
+    db.run(
+      `INSERT INTO procedures (procedure_name, procedure_cost, customer_id)
+      VALUES (?, ?, ?)`,
+      [data["procedureName"], data["procedureCost"], data["phoneNumber"]],
+      (err) => {
         if (err) {
           console.error(err);
         }
@@ -91,13 +102,12 @@ app.whenReady().then(() => {
 
   db.run(
     `CREATE TABLE IF NOT EXISTS users (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
       firstName VARCHAR(255),
       lastName VARCHAR(255),
       address VARCHAR(255),
       state VARCHAR(255),
       district VARCHAR(255),
-      phoneNumber VARCHAR(255) UNIQUE,
+      phoneNumber VARCHAR(255) PRIMARY KEY,
       time VARCHAR(255),
       date VARCHAR(255),
       pincode VARCHAR(255),
@@ -105,10 +115,16 @@ app.whenReady().then(() => {
       age VARCHAR(20),
       billNumber VARCHAR(100),
       initials VARCHAR(20),
-      newPrice VARCHAR(255)
+    )
+
+    CREATE TABLE IF NOT EXISTS procedures (
+      procedure_id INTEGER PRIMARY KEY AUTOINCREMENT,
+      procedure_name VARCHAR(255),
+      procedure_cost VARCHAR(255) NOT NULL,
+      customer_id VARCHAR(255) FOREIGN KEY REFERENCES users(phoneNumber)
     )
     `,
-    (res, err) => {
+    (err) => {
       if (err) {
         console.error(err);
       } else {
@@ -117,16 +133,16 @@ app.whenReady().then(() => {
     }
   );
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createMainWindow();
     }
   });
 });
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    db.close(err => {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    db.close((err) => {
       if (err) {
         console.error(err);
       }
